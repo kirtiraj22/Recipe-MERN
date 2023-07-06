da5c7a927186485757cc8ac65f272ed9812f5795
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-07-06 22:40:34 +0530

    added registration API endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import { userRouter } from "./routes/users.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-mongoose.connect(process.env.MONGOOSE_URI);
+app.use("/auth", userRouter);
+mongoose.connect(process.env.MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(3001, () => console.log("Server started"));
